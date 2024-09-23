164b6ee9d41bcfad9673a51f2b7fafbf4e648279
vladimir92i <vladimir.boussekeyt@gmail.com>
2024-09-23 15:45:36 +0200

    delete vue, added react

--- config/bundles.php
+++ config/bundles.php
@@ -3,5 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
-    Symfony\UX\Vue\VueBundle::class => ['all' => true],
+    Symfony\UX\React\ReactBundle::class => ['all' => true],
 ];
