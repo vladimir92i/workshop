3ccfa965d245f9291970c490b8f60450e0fbbf98
vladimir92i <vladimir.boussekeyt@gmail.com>
2024-09-23 15:39:33 +0200

    cest bon

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Vue\VueBundle::class => ['all' => true],
 ];
